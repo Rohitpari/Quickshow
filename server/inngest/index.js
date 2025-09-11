656968c37657cfb1e79232d660b4d7557c4b31f6
Rohitpari <rohitprihar7049@gmail.com>
2025-09-11 17:12:48 +0530

    ;wl

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,17 +1,15 @@
 // import { User } from "@clerk/express";
-import {User} from "../models/User.js";
+import { User } from "../models/User.js";
 import { Inngest } from "inngest";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
-//Inngest Function to save user to a database
-
+// Inngest Function to save user to a database
 const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
@@ -22,8 +20,7 @@ const syncUserCreation = inngest.createFunction(
   }
 );
 
-//Inngest Function to delete user from database
-
+// Inngest Function to delete user from database
 const syncUserdeletion = inngest.createFunction(
   { id: "delete-user-with-clerk" },
   { event: "clerk/user.deleted" },
@@ -33,22 +30,23 @@ const syncUserdeletion = inngest.createFunction(
   }
 );
 
-//Inngest Function to update user data in atabase
-
+// Inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-with-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
-    const userData = {
-      _id: id,
-      email: email_addresses[0].email_address,
-      name: first_name + " " + last_name,
-      Image: image_url,
-    };
-    // await User.FindByIdAndUpdate()
-    await User.findByIdAndUpdate(id, userData);
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    // update without trying to change _id
+    await User.findByIdAndUpdate(
+      id,
+      {
+        email: email_addresses[0].email_address,
+        name: first_name + " " + last_name,
+        Image: image_url,
+      },
+      { new: true } // optional: returns updated doc
+    );
   }
 );
 
